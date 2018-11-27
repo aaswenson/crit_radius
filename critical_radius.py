17f040ced90713a10d587b9590f314c09d076d66
Alexander Swenson <aaswenson@wisc.edu>
2018-11-27 10:58:33 -0600

    switching to bisection-like algorithm to more strategically select my domain

--- critical_radius.py
+++ critical_radius.py
@@ -1,23 +1,15 @@
+import sys
+import os
 import numpy as np
-from string import Template
-from scipy import interpolate
 from scipy.optimize import minimize_scalar, curve_fit
 from subprocess import call, DEVNULL
-import math
-import os
 from mcnp_inputs import HomogeneousInput
-import argparse
+import fit_data as fd
 
-fp = open('base_input.txt', 'r')
-lines = fp.read()
-fp.close()
-
-temp = Template(lines)
 target_keff = 1.01
+# critical radius range to sweep
+domain = (5, 28.75)
 
-global core_mass, refl_mass
-
-save_keff = open('keff_res.txt', 'w')
 g_to_kg = 0.001
 
 opt_refl_mult = {'UN'  : {'CO2' : 0.3079, 'H2O' : 0.2887},
@@ -31,7 +23,7 @@ def calc_keff(config):
     radius = config['core_r']
 
     basename = "{0}_{1}.i".format(round(frac,5), round(radius,5))
-    write_inp(radius, basename, config)
+    write_inp(basename, config)
     call(["mcnp6", "n= {0} tasks 8".format(basename)], stdout=DEVNULL)
     keff = parse_output(basename)
     os.remove('{0}r'.format(basename))
@@ -58,17 +50,13 @@ def parse_output(basename):
 
     return keff, stdv
 
-def write_inp(core_r, basename, configuration):
+def write_inp(basename, configuration):
     """Write mcnp input for keff.
     """
-    global core_mass, refl_mass
-
-    configuration['core_r'] = core_r
     input = HomogeneousInput(config=configuration)
     homog_comp = input.homog_core()
     input.write_input(basename)
     
-
 def cubic(x, a, b, c, d):
     """
     """
@@ -78,21 +66,15 @@ def cubic(x, a, b, c, d):
 
     return np.add(A,np.add(B,np.add(C,d)))
 
-def load_data_from_file(lines):
+def quad(x, a, b, c):
     """
     """
-    results = {}
-    for line in lines:
-        if 'fuel fraction' in line:
-            frac = float(line.split(':')[1])
-            results[frac] = {}
-        else:
-            data = [float(x) for x in line.split(',')]
-            results[frac][data[0]] = data[1]
-    
-    return results
+    A = a*np.power(x,2)
+    B = b*x
 
-def crit_radius(config, range, load_data=None):
+    return np.add(A,np.add(B,c))
+
+def crit_radius(config, steps):
     """Get critical radius = f(fuel_frac)
     """
 
@@ -103,33 +85,51 @@ def crit_radius(config, range, load_data=None):
     x = []
     keff = []
     
-    if load_data:
-        for radius in load_data:
-            x.append(radius)
-            keff.append(load_data[radius])
+    resfile = open('{0}_fits.csv'.format(name), '+a')
+    resfile.write('fuel fraction: {0:.2f}\n'.format(config['fuel_frac']))
+    
+    mid_r = (domain[1] + domain[0]) / 2
+    config['core_r'] = mid_r
+    kmid, stdvmid = calc_keff(config)
+    
+    if kmid > target_keff:
+        lower_r = domain[0]
+        upper_r = mid_r
+        radii = np.linspace(lower_r, upper_r, steps)[:-1]
     else:
-        resfile = open('{0}_fits.csv'.format(name), '+a')
-        resfile.write('fuel fraction: {0:.2f}\n'.format(config['fuel_frac']))
-        for radius in np.linspace(range[0], range[1], range[2]):
-            config['core_r'] = radius
-            res, stdv = calc_keff(config)
-            x.append(radius)
-            keff.append(res)
-            resfile.write('{0},{1},{2}\n'.format(radius, res, stdv))
-        
+        lower_r = mid_r
+        upper_r = domain[1]
+        radii = np.linspace(lower_r, upper_r, steps)[1:]
+        x.append(lower_r)
+        keff.append(kmid)
+        resfile.write('{0},{1},{2}\n'.format(lower_r, kmid, stdvmid))
+
+    for radius in radii:
+        config['core_r'] = radius
+        res, stdv = calc_keff(config)
+        x.append(radius)
+        keff.append(res)
+        resfile.write('{0},{1},{2}\n'.format(radius, res, stdv))
+
+    if kmid > target_keff:
+        x.append(upper_r)
+        keff.append(kmid)
+        resfile.write('{0},{1},{2}\n'.format(upper_r, kmid, stdvmid))
+    
     keff_reduced = np.array(keff) - target_keff
-    coeffs, copt = curve_fit(cubic, x, keff_reduced)
+    coeffs, copt = curve_fit(quad, x, keff_reduced)
     
     roots = np.roots(coeffs)
     real_roots = roots[np.isreal(roots)].real
+    real_roots = [x for x in real_roots if x >= 0]
+    
     # return only the real root
     print(coolant, fuel, config['fuel_frac'])
-    print(real_roots)
-    real_roots = [x for x in real_roots if x >= 0]
+    print(min(real_roots))
 
     return min(real_roots)
     
-def fuel_frac(coolant, fuel, clad, matr, load_data=True):
+def fuel_frac(coolant, fuel, clad, matr):
     """Determine the optimal reflector thickness for a given reactor
     configuration.
     """
@@ -145,77 +145,44 @@ def fuel_frac(coolant, fuel, clad, matr, load_data=True):
     results = open('{0}_{1}_results.txt'.format(coolant, fuel) , '+w')
     results.write('fuel_frac,crit_radius\n') 
     
-    data = None
-
-    for frac in np.arange(0.1, 1, 0.1):
+    for frac in np.arange(0.6, 1, 0.1):
         config['fuel_frac'] = frac
-        # load saved results
-        if load_data:
-            filename = '{0}_{1}_fits.csv'.format(coolant, fuel)
-            lines = open(filename, 'r').readlines()
-            keff_data = load_data_from_file(lines)
-            data = keff_data[round(frac, 1)]
+        config['ref_mult'] = refl_mult(config)
         # get critical radius
-        r = crit_radius(config, [5, 70, 5], data)
-        results.write('{0:.2f},{1}\n'.format(frac, r))
+        r = crit_radius(config, 5)
+        results.write('{0:.2f},{1:.5f},{2:.5f}\n'.format(frac, r,
+                                                         config['ref_mult']))
 
     results.close()
 
-def refl_mult(coolant, fuel, clad, matr, load_data=True):
+def refl_mult(config):
     """Determine the optimal reflector thickness for a given reactor
     configuration.
     """
 
-    rhos = {'CO2' : 252.638e-3, 'H2O' : 141.236e-3}
-    config = {'fuel' : fuel,
-              'matr' : matr,
-              'cool' : coolant,
-              'clad' : clad,
-              'rho_cool' : rhos[coolant],
-              'fuel_frac' : 0.6
-             }
-    
-    results = open('{0}_{1}_results.txt'.format(coolant, fuel) , '+w')
-    results.write('fuel_frac,crit_radius\n') 
-    
-    data = None
+    mults = np.linspace(0.001, 0.5, 5)
+    data = {'mass' : [], 'mult' : mults}
 
-    for mult in np.arange(0.001, 0.4, 0.05):
+    for mult in mults:
         config['ref_mult'] = mult
-        # load saved results
-        if load_data:
-            filename = '{0}_{1}_fits.csv'.format(coolant, fuel)
-            lines = open(filename, 'r').readlines()
-            keff_data = load_data_from_file(lines)
-            data = keff_data[round(mult, 1)]
         # get critical radius
-        r = crit_radius(config, [5, 70, 5], data)
+        r = crit_radius(config, 5)
 
         config['core_r'] = r
         input = HomogeneousInput(config=config)
         homog_comp = input.homog_core()
+        
+        data['mass'].append(input.core_mass + input.refl_mass)
     
-        m_tot = input.core_mass + input.refl_mass
-        res_str = '{0:.2f},{1:.3f},{2:.3f}\n'.format(mult, r, m_tot)
-        results.write(res_str)
-        print(res_str)
-
-    results.close()
+    popt, cov = fd.fit_data(data, cubic, 'mult', 'mass')
+    opt_mult = fd.min_mult(popt)
+    
+    return opt_mult
 
 if __name__ == '__main__':
-   load = False
-   fuel_frac('CO2', 'UO2', 'Inconel-718', None, load)
-   fuel_frac('H2O', 'UO2', 'Inconel-718', None, load)
-   fuel_frac('CO2', 'UN',  'Inconel-718', 'W', load)
-   fuel_frac('H2O', 'UN',  'Inconel-718', 'W', load)
-   save_keff.close()
-    # reflector thickness
-#   load = False
-#   refl_mult('CO2', 'UO2', 'Inconel-718', None, load)
-#   refl_mult('H2O', 'UO2', 'Inconel-718', None, load)
-#   refl_mult('CO2', 'UN',  'Inconel-718', 'W', load)
-#   refl_mult('H2O', 'UN',  'Inconel-718', 'W', load)
-#   save_keff.close()
-
-
+    matr = None
+    cool, fuel, clad = sys.argv[1:4]
+    if len(sys.argv) == 5:
+        matr = sys.argv[4]
+    fuel_frac(cool, fuel, clad, matr)
 
